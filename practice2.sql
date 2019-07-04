4526580fec670962c6cf926bf54272607d1d1803
LIShuning <lishiuning1955142623@outlook.com>
2019-07-04 14:31:10 +0800

    practice3 completed

--- practice2.sql
+++ practice2.sql
@@ -3,4 +3,4 @@
 
 SELECT companyName, `name` FROM 
   (SELECT employee.*, company.companyName FROM Employee LEFT JOIN Company ON Employee.companyId = Company.id) employee Right JOIN 
-  (SELECT companyId, max(salary) AS maxval FROM Employee GROUP BY companyId) t ON t.maxval = employee.salary AND employee.companyId = t.companyId;
+  (SELECT companyId, max(salary) AS maxval FROM Employee GROUP BY companyId) t ON t.maxval = employee.salary AND employee.companyId = t.companyId;\
--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,7 @@
 #取得平均薪水最高的公司
+
+SELECT companyId, avg(salary) AS avgSalary from employee GROUP BY employee.companyId ORDER BY avgSalary DESC LIMIT 1;
+
 #输出公司名称和平均薪水：companyName avgSalary
 
-
+SELECT companyName, avgSalary FROM (SELECT companyId, avg(salary) AS avgSalary FROM Employee GROUP BY companyId) A INNER JOIN Company B ON A.companyId=B.id
