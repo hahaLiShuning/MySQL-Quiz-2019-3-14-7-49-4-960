a522c0e5b7971f32add9658a2c286a2f506d5ad9
LIShuning <lishiuning1955142623@outlook.com>
2019-07-04 14:01:55 +0800

    practice2 completed

--- practice2.sql
+++ practice2.sql
@@ -1,2 +1,6 @@
 # 取得每个company中最高薪水的人员名字
 # 输出结果包含公司名称和人员名称：companyName name
+
+SELECT companyName, `name` FROM 
+  (SELECT employee.*, company.companyName FROM Employee LEFT JOIN Company ON Employee.companyId = Company.id) employee Right JOIN 
+  (SELECT companyId, max(salary) AS maxval FROM Employee GROUP BY companyId) t ON t.maxval = employee.salary AND employee.companyId = t.companyId;
